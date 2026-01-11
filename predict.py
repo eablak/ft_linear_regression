f4c2f7086955a85ede8f52bafa67921e9fb55548
eablak <esrablk9@gmail.com>
2026-01-11 14:03:24 +0300

    train and predict formulas

--- predict.py
+++ predict.py
@@ -1,1 +1,33 @@
-# estimateP rice(mileage) = θ0 + (θ1 ∗ mileage)
+# estimatePrice(mileage) = θ0 + (θ1 ∗ mileage)
+import sys
+
+
+def get_parameters():
+    try:
+        file = open('output.txt')
+        content = file.readlines()
+
+        theta0 = content[0].strip()
+        theta1 = content[1].strip()
+    except:
+        theta0, theta1 = 0, 0
+    
+    return theta0, theta1
+
+
+def estimatePrice(theta0, theta1, milage):
+    return theta0 + (theta1 * milage)
+    
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        sys.exit("Write your milage as a argument!")
+
+    try:
+        float(sys.argv[1])
+    except:
+        sys.exit("Write only float")
+
+    theta0, theta1 = get_parameters()
+    print(estimatePrice(float(theta0), float(theta1), float(sys.argv[1])))
--- training.py
+++ training.py
@@ -1,69 +1,68 @@
-# tmpθ0 = learningRate ∗ 1/m   m−1∑i=0(estimateP rice(mileage[i]) − price[i])
-# tmpθ1 = learningRate ∗ 1/m   m−1∑i=0(estimateP rice(mileage[i]) − price[i]) ∗ mileage[i]
-
+# tmpθ0 = learningRate ∗ 1/m   m−1∑i=0(estimatePrice(mileage[i]) − price[i])
+# tmpθ1 = learningRate ∗ 1/m   m−1∑i=0(estimatePrice(mileage[i]) − price[i]) ∗ mileage[i]
 import pandas
 import sys
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import minmax_scale
+import numpy as np
 
 
 def get_dataset():
     try:
         return pandas.read_csv('dataset/data.csv')
-        
     except:
         sys.exit("No dataset")
 
 
-def plotting_data(dataset):
-    plt.scatter(dataset.km, dataset.price)
-    plt.show()
+def normalize_dataset(dataset):
+
+    normalized = dataset.copy()
+    normalized['km'] = minmax_scale(dataset['km'])
+    return normalized
 
 
-def loss_function(m, b, points):
-    total_error = 0
-    for i in range(len(points)):
-        x = points.iloc[i].km
-        y = points.iloc[i].price
-        total_error += (y - (m*x + b)) **2
-    total_error / float(len(points)) 
+def train_model(theta0, theta1, learning_rate, epochs, X, y):
 
+    m = len(X)
 
-def gradient_descent(m_now, b_now, points, L):
-    m_gradient = 0
-    b_gradient = 0
-
-    n = len(points)
-
-    for i in range(n):
-        x = points.iloc[i].km
-        y = points.iloc[i].price
-
-        m_gradient += -(2/n) * x * (y - (m_now * x + b_now))
-        b_gradient += -(2/n) * (y - (m_now * x + b_now))
-
-    m = m_now - L * m_gradient
-    b = b_now - L * b_gradient
-
-    return m, b
-
-
-def trainModel(m, b, learningRate, epochs, dataset):
     for i in range(epochs):
-        m, b = gradient_descent(m, b, dataset, learningRate)
+        prediction = theta0 + (theta1 * X)
+        error = prediction - y
 
-    print(m, b)
+        tmp_theta0 = (1/m) * np.sum(error)
+        tmp_theta1 = (1/m) * np.sum(error*X)
 
-def plotResult(dataset):
-    plt.scatter(dataset.km, dataset.price, color="black")
-    plt.plot(color="red")
-    plt.show()
+        theta0 -= learning_rate * tmp_theta0
+        theta1 -= learning_rate * tmp_theta1
+
+    return (theta0, theta1)
+
+
+def denormalize_theta(normalized_theta0, normalized_theta1, dataset):
+    theta1 = normalized_theta1 / (dataset['km'].max() - dataset['km'].min())
+    theta0 = normalized_theta0 - (theta1 * dataset['km'].min())
+
+    return theta0, theta1
+
+
+def save_into_file(theta0, theta1):
+    with open("output.txt", "w") as f:
+        f.write(str(theta0) + "\n" + str(theta1))
 
 
 
 if __name__ == "__main__":
-    dataset = get_dataset()
-    # plotting_data(dataset)
-    trainModel(0, 0, 0.0001, 1000, dataset)
-    # plotResult(dataset)
 
-    
+    theta0 = 0
+    theta1 = 0
+    learning_rate = 0.001
+    epochs = 10000
+
+    dataset = get_dataset()
+    normalized_dataset = normalize_dataset(dataset)
+    X = normalized_dataset.iloc[:, 0].values
+    y = dataset.iloc[:, 1].values
+
+    normalized_theta0, normalized_theta1 = train_model(theta0, theta1, learning_rate, epochs, X, y)
+    theta0, theta1 = denormalize_theta(normalized_theta0, normalized_theta1, dataset)
+    save_into_file(theta0, theta1)
